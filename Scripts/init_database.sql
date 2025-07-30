70a28f160299ee8fc3b48784d3106815c6332155
Farhan khan <farhanriyaz9005@gmail.com>
2025-07-30 21:46:48 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,1 +1,22 @@
+-- ============================================
+-- Script: Setup for Data Warehouse Project
+-- Purpose: Create database and schemas for ETL pipeline
+-- Author: Farhan Khan
+-- Date: 2025-07-29
 
+-- 🛠️ Create the database (Run this from the "Databases" level, not inside another DB)
+CREATE DATABASE Data_warehouse;
+-- =============================================
+-- ⬇️ The following should be run inside data_warehouse database
+-- =============================================
+-- 🔁 Switch to the new database before running this (in pgAdmin: open new query in data_warehouse)
+
+
+-- 📂 Create Bronze Layer Schema (Raw Data)
+CREATE SCHEMA bronze;
+
+-- 📂 Create Silver Layer Schema (Cleaned Data)
+CREATE SCHEMA silver;
+
+-- 📂 Create Gold Layer Schema (Aggregated/Final Data)
+CREATE SCHEMA gold;
